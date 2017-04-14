4d3a53714afff4d19eeadecdebe4def46b0151de
yangbo <yangbo@nicaifu.com>
2017-04-14 19:21:14 +0800

    MOD:   28. Implement strStr()

--- src/com/training/algorithm/string/Solution_28.java
+++ src/com/training/algorithm/string/Solution_28.java
@@ -22,38 +22,37 @@ public class Solution_28 {
       /*  String haystack = "salssskassshasldh";
         String needle = "asss";*/
         String haystack = "mississippi";
-        String needle =   "issip";
+        String needle = "issip";
         System.out.println("" + solution.strStr(haystack, needle));
     }
 
     public int strStr(String haystack, String needle) {
-            int startIndex = -1;
+        int startIndex = -1;
 
-            if (haystack == null || needle == null) {
-                return 0;
-            }
-            if ( needle.length() == 0) {
-                return 0;
-            }
+        if (haystack == null || needle == null) {
+            return 0;
+        }
+        if (needle.length() == 0) {
+            return 0;
+        }
 
-            for(int i=0; i<haystack.length(); i++){
-                if(i + needle.length() > haystack.length())
-                    return -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (i + needle.length() > haystack.length())
+                return -1;
 
-                int m = i;
-                for(int j=0; j<needle.length(); j++){
-                    if(needle.charAt(j)==haystack.charAt(m)){
-                        if(j==needle.length()-1)
-                            return i;
-                        m++;
-                    }else{
-                        break;
-                    }
-
+            int m = i;
+            for (int j = 0; j < needle.length(); j++) {
+                if (needle.charAt(j) == haystack.charAt(m)) {
+                    if (j == needle.length() - 1)
+                        return i;
+                    m++;
+                } else {
+                    break;
                 }
-            }
 
-            return -1;
+            }
+        }
+        return -1;
     }
 
 }
