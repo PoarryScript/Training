d7d67443a106f894b16ec7c2a76b122e03dd284c
yangbo <yangbo@nicaifu.com>
2017-04-18 10:16:17 +0800

    MOD:  98  Validate Binary Search Tree

--- src/com/training/algorithm/tree/Solution_98.java
+++ src/com/training/algorithm/tree/Solution_98.java
@@ -1,5 +1,9 @@
 package com.training.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Created by j-yangbo on 2017/4/10.
  * <p>
@@ -8,7 +12,7 @@ package com.training.algorithm.tree;
 public class Solution_98 {
     public static void main(String[] args) {
         Solution_98 solution = new Solution_98();
-        TreeNode root = new TreeNode(10);
+       /*  TreeNode root = new TreeNode(10);
         TreeNode left = new TreeNode(5);
         TreeNode right = new TreeNode(15);
         root.left = left;
@@ -16,47 +20,91 @@ public class Solution_98 {
         TreeNode lright = new TreeNode(6);
         TreeNode llright = new TreeNode(20);
         right.left = lright;
-        lright.right = llright;
+        lright.right = llright;*/
+
+        TreeNode root = new TreeNode(2);
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
 
 //        solution.inOrder(root);
 
         boolean isValid = solution.isValidBST(root);
         System.out.println("isValid:" + isValid);
 
+        solution.inOrderIteration(root);
     }
 
-    int rootValue;
 
+    /**
+     * 由于树的定义是递归的，所以只需要判断当前的节点是否在一个取值区间
+     *
+     * @param root
+     * @return
+     */
+  /*  public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    public boolean isValid(TreeNode node, double min, double max) {
+        if (node == null) {
+            return true;
+        }
+        if(node.val <= min || node.val >= max ){
+            return false;
+        }
+        return  isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+    }*/
+
+
+    /**
+     * 根据BST的属性 中序遍历 判断节点大小递增则为valid
+     * @param root
+     * @return
+     */
     public boolean isValidBST(TreeNode root) {
-
-
         if (root == null) {
             return true;
         }
-        rootValue = root.val;
-        return viaNode(root.val, root.left, root.right);
-    }
+        List<Integer> nodeList = inOrderIteration(root);
 
-    private boolean viaNode(int val, TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
-            return true;
-        }
-        if (left == null && right != null) {
-            if (right.val <= val || right.val <= rootValue) {
+        for (int i = 0; i < nodeList.size()-1; i++) {
+            if (nodeList.get(i)>=nodeList.get(i+1)){
                 return false;
             }
-            return viaNode(right.val, right.left, right.right);
-        }
-        if (right == null && left != null) {
-            if (left.val >= val || left.val >= rootValue) {
-                return false;
-            }
-            return viaNode(left.val, left.left, left.right);
-        }
-        if (left != null && right != null) {
-            return viaNode(right.val, right.left, right.right) && viaNode(left.val, left.left, left.right);
         }
         return true;
+
+    }
+
+
+    private List<Integer> inOrderIteration(TreeNode treeNode) {
+        if (treeNode == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> nodeList =new ArrayList<>();
+        Stack<TreeNode> unVisitNode = new Stack<>();
+        TreeNode node = treeNode;
+
+        while (node != null) {
+            unVisitNode.push(node);
+            node = node.left;
+        }
+        while (unVisitNode.size() > 0) {
+            node = unVisitNode.pop();
+//            System.out.println("nodeValue-->" + node.val);
+            nodeList.add(node.val);
+            if (node.right != null) {
+                node = node.right;
+
+                while (node != null) {
+                    unVisitNode.push(node);
+                    node = node.left;
+                }
+            }
+        }
+        return nodeList;
     }
 
     private void inOrder(TreeNode treeNode) {
