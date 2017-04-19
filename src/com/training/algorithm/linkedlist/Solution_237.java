1af23e2d3f5854f5c82dc934abfcd0a6322e0d21
yangbo <yangbo@nicaifu.com>
2017-04-19 10:37:21 +0800

    ADD:  237. Delete Node in a Linked List

---
+++ src/com/training/algorithm/linkedlist/Solution_237.java
@@ -0,0 +1,37 @@
+package com.training.algorithm.linkedlist;
+
+/**
+ * Created by j-yangbo on 2017/4/19.
+ * <p>
+ * 237. Delete Node in a Linked List
+ * <p>
+ * <p>
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ * <p>
+ * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+ */
+public class Solution_237 {
+
+    public void deleteNode(ListNode node) {
+
+        if (node == null) {
+            return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+
+        node.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+
+
+    }
+}
